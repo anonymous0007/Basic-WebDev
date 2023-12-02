a0cdec8bbbec12d0e105d89b88aa9c2220246fec
anonymous0007 <creat6823@gmail.com>
2023-12-02 12:31:21 +0000

    some loop & list call

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)  
 
+JOBS = [
+  {
+    'id':1,
+    'title':'Spoofing',
+    'price':'50$'
+  },
+  {
+    'id':2,
+    'title':'Carding',
+    'price':'70$'
+  },
+  {
+    'id':3,
+    'title':'Hijacking',
+    'price':'410$'
+  },
+  {
+    'id':4,
+    'title':'Data-recovery',
+    'price':'200$'
+  }
+]
+
 @app.route("/")
 def home():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
     
 
 if __name__ == '__main__':
